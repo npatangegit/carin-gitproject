d49dc835c1d6641cb9ac3a76cd3bb8e84bcf1742
Nitisha Patange <nitishapatange1997@gmail.com>
2021-07-25 21:46:26 -0500

    Completed teams model

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
     path("about", views.about, name='about'),
     path("services", views.services, name='services'),
     path("contact", views.contact, name='contact'),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
