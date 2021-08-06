d21489eacd87965751874748e6013b6ad1bcb2cc
Nitisha Patange <nitishapatange1997@gmail.com>
2021-08-06 00:35:45 -0500

    Made multiple changes

--- pages/urls.py
+++ pages/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     path("about", views.about, name='about'),
     path("services", views.services, name='services'),
     path("contact", views.contact, name='contact'),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
